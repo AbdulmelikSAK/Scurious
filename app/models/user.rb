e3cbfa3a8b3a97833b123a025b1db8d230ae61f0
Abdulmelik SAK <melik.sak@icloud.com>
2020-11-16 17:59:47 +0100

    users validations and devise views

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates :profile, presence: true
+  validates :role, presence: true, inclusion: { in: ["creator", "developper", "angel"] }
+end
