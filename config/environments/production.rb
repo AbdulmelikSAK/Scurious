4669d2b032a34415ccf250571f829814dcb9c03e
Abdulmelik SAK <melik.sak@icloud.com>
2020-11-16 17:25:09 +0100

    ajout de devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+   config.action_mailer.default_url_options = { host: 'https://scurious.herokuapp.com/'}
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
