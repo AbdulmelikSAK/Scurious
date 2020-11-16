41c393afc58714b807360def611b5db728d74612
Abdulmelik SAK <melik.sak@icloud.com>
2020-11-16 17:20:35 +0100

    rails new + rputes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'projects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projects, except: :delete do
+    resources :applications, only: [:new, :create]
+  end
 end
