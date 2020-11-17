a86211a2e6363f245029a2288f9c966a44362596
Abdulmelik SAK <melik.sak@icloud.com>
2020-11-17 11:22:11 +0100

    Creation de seed pour les tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:profile, :role])
 
-    # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:profile])
   end
 end
