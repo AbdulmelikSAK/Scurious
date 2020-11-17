61c123fa096ca4340ff082321e8fd841cd122447
Mailys Bouin <d.syliam@gmail.com>
2020-11-17 11:29:36 +0100

    homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_163434) do
+ActiveRecord::Schema.define(version: 2020_11_16_172723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candidates", force: :cascade do |t|
+    t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
+    t.string "status"
+    t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_candidates_on_project_id"
+    t.index ["user_id"], name: "index_candidates_on_user_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "dev_id"
+    t.integer "angel_id"
+    t.string "title"
+    t.string "description"
+    t.string "evolution"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +52,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_163434) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "candidates", "projects"
+  add_foreign_key "candidates", "users"
+  add_foreign_key "projects", "users"
 end
