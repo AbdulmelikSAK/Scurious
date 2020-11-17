7c40351180c20574e58689f3ec655519d1792133
Diane Marino <dianemarino0@gmail.com>
2020-11-17 14:09:53 +0100

    new project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,30 @@
 class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
+
   def index
 
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: 'A new project was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+    def project_params
+      params.require(:project).permit(:title, :description)
+    end
 end
