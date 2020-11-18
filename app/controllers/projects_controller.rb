fe8d23f00e8e15082104e0c93f6fcad5da3c286d
Diane Marino <dianemarino0@gmail.com>
2020-11-18 17:14:13 +0100

    evolution

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -51,6 +51,6 @@ class ProjectsController < ApplicationController
   private
 
   def project_params
-    params.require(:project).permit(:title, :description)
+    params.require(:project).permit(:title, :description, :evolution)
   end
 end
