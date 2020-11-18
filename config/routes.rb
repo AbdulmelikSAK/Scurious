a06a53b1e30bc87b66fbeabe773713d0149e31fa
Mailys Bouin <d.syliam@gmail.com>
2020-11-18 15:33:47 +0100

    creation controller + routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update]
+  get 'dashboard', to: 'users#dashboard', as: :dashboard
+
   resources :projects, except: [:destroy, :index] do
     resources :candidates, only: [:new, :create]
   end
